28291b376650b7dadfb0a3d8a49a9d419179e152
dingjiefeng <helloworldjf@163.com>
2018-05-23 22:39:20 +0800

    add googletest

---
+++ lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jeff/Desktop/cppDemo/web Server/base_tests/googletest/googlemock
+# Build directory: /home/jeff/Desktop/cppDemo/web Server/base_tests/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ lib/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jeff/Desktop/cppDemo/web Server/base_tests/googletest/googletest
+# Build directory: /home/jeff/Desktop/cppDemo/web Server/base_tests/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
