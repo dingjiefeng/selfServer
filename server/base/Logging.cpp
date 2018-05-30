7772243d3b6efdc651d498333e19801b137f0ce4
dingjiefeng <helloworldjf@163.com>
2018-05-30 22:01:39 +0800

    add Logging.h/cpp

---
+++ server/base/Logging.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jeff on 18-5-30.
+//
+
+#include "Logging.h"
---
+++ server/base/Logging.h
@@ -0,0 +1,10 @@
+//
+// Created by jeff on 18-5-30.
+//
+
+#ifndef WEBSERVER_LOGGING_H
+#define WEBSERVER_LOGGING_H
+
+#include "base.h"
+
+#endif //WEBSERVER_LOGGING_H
