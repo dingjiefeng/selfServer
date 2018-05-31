59e58df69e4a5c7bd5d051fb4a02a21bd35958ef
dingjiefeng <helloworldjf@163.com>
2018-05-31 22:33:34 +0800

    添加logger

--- server/base/Logging.cpp
+++ server/base/Logging.cpp
@@ -2,4 +2,42 @@
 // Created by jeff on 18-5-30.
 //
 
+#include <sys/time.h>
 #include "Logging.h"
+
+
+namespace selfServer
+{
+
+    void output(const char* msg, int len)
+    {
+
+    }
+
+    Logger::Impl::Impl(const char *file, int line)
+        : m_stream(),
+          m_basename(file),
+          m_line(line)
+    {}
+
+    void Logger::Impl::formatTime()
+    {
+        struct timeval tv;
+        time_t time;
+        char str_t[26] = {0};
+        gettimeofday(&tv, nullptr);
+        time = tv.tv_sec;
+        struct tm* p_time = localtime(&time);
+        strftime(str_t, 26, "%Y-%m-%d %H:%M:%S\n", p_time);
+    }
+    Logger::Logger(const char *file, int line)
+        : m_impl(file, line)
+    {}
+
+    Logger::~Logger()
+    {
+        m_impl.finish();
+        const LogStream::Buffer& buf(stream().buffer());
+        output(buf.data(), buf.length());
+    }
+}
--- server/base/Logging.h
+++ server/base/Logging.h
@@ -5,6 +5,47 @@
 #ifndef WEBSERVER_LOGGING_H
 #define WEBSERVER_LOGGING_H
 
-#include "base.h"
 
+#include <cstring>
+#include "LogStream.h"
+
+namespace selfServer
+{
+    class Logger
+    {
+    public:
+
+        Logger(const char* file, int line);
+        ~Logger();
+
+        LogStream& stream() { return m_impl.m_stream;}
+
+        typedef std::function<void(const char*, int)> OutputFunc;
+        typedef std::function<void()> FlushFunc;
+
+        static void setOutput(OutputFunc);
+        static void setFlush(FlushFunc);
+
+    private:
+
+        class Impl
+        {
+        public:
+            Impl(const char* file, int line);
+            void formatTime();
+            void finish();
+
+            LogStream m_stream;
+            int m_line;
+            std::string m_basename;
+        };
+        Impl m_impl;
+    };
+
+
+
+#define LOG selfServer::Logger(__FILE__, __LINE__).stream()
+
+
+}
 #endif //WEBSERVER_LOGGING_H
