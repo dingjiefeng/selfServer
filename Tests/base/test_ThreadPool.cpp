6629668a8b2467b8c97550bbe7be8a8cb3c2b602
dingjiefeng <helloworldjf@163.com>
2018-06-06 22:35:36 +0800

    add the unit test of ThreadPool

---
+++ Tests/base/test_ThreadPool.cpp
@@ -0,0 +1,54 @@
+//
+// Created by jeff on 18-6-6.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../../server/base/ThreadPool.h"
+
+using namespace selfServer;
+using testing::Eq;
+
+class TestThreadPool : public testing::Test
+{
+public:
+    TestThreadPool() = default;
+
+};
+
+TEST_F(TestThreadPool, demo)
+{
+    std::mutex mtx;
+    try
+    {
+        ThreadPool tp;
+        std::vector<std::future<int>> v;
+        std::vector<std::future<void>> v1;
+        for (int i = 0; i <= 10; ++i)
+        {
+            auto ans = tp.add([](int answer) { return answer; }, i);
+            v.push_back(std::move(ans));
+        }
+        for (int i = 0; i <= 5; ++i)
+        {
+            auto ans = tp.add([&mtx](const std::string& str1, const std::string& str2)
+                              {
+                                  std::lock_guard<std::mutex> lg(mtx);
+                                  std::cout << (str1 + str2) << std::endl;
+                                  return;
+                              }, "hello ", "world");
+            v1.push_back(std::move(ans));
+        }
+        for (auto &i : v) {
+            std::lock_guard<std::mutex> lg(mtx);
+            std::cout << i.get() << std::endl;
+        }
+        for (auto &i : v1) {
+            i.get();
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
