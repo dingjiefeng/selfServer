96adb6f8148695c64ecd2e6746af0e24d566155b
dingjiefeng <helloworldjf@163.com>
2018-06-06 22:11:29 +0800

    add class any like boost::any

--- server/base/base.h
+++ server/base/base.h
@@ -71,4 +71,118 @@ private:
     std::chrono::steady_clock::time_point pre_t;
 
 };
+
+namespace selfServer
+{
+    class any
+    {
+    public: // structors
+
+        any() : content(0)
+        {
+        }
+
+        template<typename ValueType>
+        any(const ValueType & value)
+                : content(new holder<ValueType>(value))
+        {
+        }
+
+        any(const any & other)
+                : content(other.content ? other.content->clone() : 0)
+        {
+        }
+
+        ~any()
+        {
+            delete content;
+        }
+
+    public: // modifiers
+
+        any & swap(any & rhs)
+        {
+            std::swap(content, rhs.content);
+            return *this;
+        }
+
+        template<typename ValueType>
+        any & operator=(const ValueType & rhs)
+        {
+            any(rhs).swap(*this);
+            return *this;
+        }
+
+        any & operator=(any rhs)
+        {
+            rhs.swap(*this);
+            return *this;
+        }
+
+    public: // queries
+
+        bool empty() const
+        {
+            return !content;
+        }
+
+        const std::type_info & type() const
+        {
+            return content ? content->type() : typeid(void);
+        }
+
+        class placeholder
+        {
+        public: // structors
+
+            virtual ~placeholder()
+            {
+            }
+
+        public: // queries
+
+            virtual const std::type_info & type() const = 0;
+
+            virtual placeholder * clone() const = 0;
+
+        };
+
+        template<typename ValueType>
+        class holder : public placeholder
+        {
+        public: // structors
+
+            holder(const ValueType & value)
+                    : held(value)
+            {
+            }
+
+        public: // queries
+
+            virtual const std::type_info & type() const
+            {
+                return typeid(ValueType);
+            }
+
+            virtual placeholder * clone() const
+            {
+                return new holder(held);
+            }
+
+        public: // representation
+
+            ValueType held;
+
+        private: // intentionally left unimplemented
+            holder & operator=(const holder &);
+        };
+
+    public: // representation (public so any_cast can be non-friend)
+
+
+        placeholder* content;
+
+    };
+}
+
 #endif //WEBSERVER_BASE_H
