b115e39573a26eac60bbd229892c35d90809a4df
dingjiefeng <helloworldjf@163.com>
2018-06-08 17:36:19 +0800

    添加channel的单元测试

---
+++ Tests/net/test_Channel.cpp
@@ -0,0 +1,24 @@
+//
+// Created by jeff on 18-6-8.
+//
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../../server/net/Channel.h"
+
+using namespace selfServer;
+using namespace selfServer::net;
+
+class TestChannel : public testing::Test
+{
+public:
+    TestChannel() = default;
+
+};
+
+
+TEST_F(TestChannel, channel)
+{
+
+}
+
+
